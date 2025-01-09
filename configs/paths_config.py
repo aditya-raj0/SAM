467a16b359a479fb7b091e7a8c3cd0a2c7bc01c4
aditya-raj0 <aditya@yonderwonder.ai>
2025-01-09 10:45:46 +0000

    resolved key error in the path configs

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -4,7 +4,7 @@ dataset_paths = {
 }
 
 model_paths = {
-    'pretrained_psp_encoder': 'pretrained_models/psp_ffhq_encode.pt',
+    'pretrained_psp': 'pretrained_models/psp_ffhq_encode.pt',
     'ir_se50': 'pretrained_models/model_ir_se50.pth',
     'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
     'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
